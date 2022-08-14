c1890b27e9e733564308afa5db0df86f92066281
shekharS2 <saikiashekhar25@gmail.com>
2022-08-14 19:07:43 +0530

    first commit

--- pie-chart/pie-chart.js
+++ pie-chart/pie-chart.js
@@ -1,7 +1,5 @@
 const slider = document.getElementsByClassName("slider")[0];
 const pieChart = document.getElementsByClassName("pie-chart")[0];
 slider.addEventListener("input", (e) => {
-    console.log(e.target.value);
-    // document.querySelector(".output").innerHTML = 1 
     pieChart.style = `--percentage: ${e.target.value}%`;
 });
